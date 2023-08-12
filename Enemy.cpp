633dbe3c1f3c5ce1b7d09f98c117d9ff9778a3ff
kt610 <kt610@MSI>
2023-08-12 21:09:16 +0900

    02_07b

--- Enemy.cpp
+++ Enemy.cpp
@@ -10,33 +10,25 @@ void Enemy::Initialize(Model* model, uint32_t textureHandle ) {
 
 	//因数で受け取った初期座標をセット
 	worldTransform_.translation_ = { 0,3,100 };
+
+	state = new EnemyStateApproach();
 }
 
-void Enemy::ApproachUpdate() {
-	//移動（ベクトルを加算）
-	worldTransform_.translation_.z -= velocity_.z;
-	//規定の位置まで到達したら離脱
-	if (worldTransform_.translation_.z < 0.0f) {
-		phase_ = Phase::Leave;
-	}
-}
-
-void Enemy::LeaveUpdate() {
-	//移動（ベクトルを加算）
-	worldTransform_.translation_.y += velocity_.y;
+void Enemy::Move() {
 	worldTransform_.translation_.x -= velocity_.x;
+	worldTransform_.translation_.y += velocity_.y;
+	worldTransform_.translation_.z -= velocity_.z;
+
 }
 
-//staticで宣言したメンバ関数ポインタテーブルの実態
-void(Enemy::*Enemy::phasePFuncTable[])() = { &Enemy::ApproachUpdate,&Enemy::LeaveUpdate };
 
 void Enemy::Update() {
+
+	state->Update(this);
 	//ワールドトランスフォームの更新
 	worldTransform_.UpdateMatrix();
 
-	//メンバ関数ポインタに入ってる関数を呼び出す
-	(this->*phasePFuncTable[static_cast<size_t>(phase_)])();
-
+	
 	ImGui::Begin("Enemy pos");
 	// float3入力ボックス
 	ImGui::InputFloat3("InputFloat3", &worldTransform_.translation_.x);
@@ -47,4 +39,31 @@ void Enemy::Update() {
 
 void Enemy::Draw(const ViewProjection& viewProjection) {
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
+
+
+void Enemy::SetVelocity(float x, float y, float z) {
+	velocity_.x = x;
+	velocity_.y = y;
+	velocity_.z = z;
+}
+
+void Enemy::ChangeState(BaseEnemyState* newState) { state = newState; }
+
+
+void EnemyStateApproach::Update(Enemy* pEnemy) {
+	pEnemy->SetVelocity(0, 0, 1);
+	//移動
+	pEnemy->Move();
+	//既定の位置に達したら離脱
+	if (pEnemy->GetWorldTransform().z < 0.0f) {
+		pEnemy->ChangeState(new EnemyStateLeave());
+	}
+}
+
+
+void EnemyStateLeave::Update(Enemy* pEnemy) {
+	pEnemy->SetVelocity(0.5f, 0.5f, 0.0f);
+	//移動
+	pEnemy->Move();
 }
--- Enemy.h
+++ Enemy.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "Model.h"
 #include "WorldTransform.h"
-#include "TextureManager.h"
 #include "ImGuiManager.h"
+#include "input/Input.h"
 
 #include <cassert>
+
+class Enemy;
+
+class BaseEnemyState {
+public:
+	virtual void Update(Enemy* pEnemy) = 0;
+};
+
+class EnemyStateApproach :public BaseEnemyState {
+public:
+	void Update(Enemy* pEnemy);
+};
+
+class EnemyStateLeave :public BaseEnemyState {
+public:
+	void Update(Enemy* pEnemy);
+};
+
+
 
 class Enemy {
 public:
@@ -19,22 +38,26 @@ public:
 	/// </summary>
 	void Update();
 
-	/// <summary>
-	/// 
-	/// </summary>
-	void ApproachUpdate();
-
-	void LeaveUpdate();
+	void Move();
 
 	/// <summary>
 	/// 描画
 	/// </summary>
 	void Draw(const ViewProjection& viewProjection);
 
-	enum class Phase {
-		Approach,//接近する
-		Leave,//
-	};
+
+
+	void ChangeState(BaseEnemyState* newState);
+
+	void SetVelocity(float x, float y, float z);
+	/// <summary>
+	/// 
+	/// </summary>
+	//void ApproachUpdate();
+	//void LeaveUpdate();
+
+	Vector3 GetWorldTransform() { return worldTransform_.translation_; }
+	Vector3 GetVelocity() { return velocity_; }
 
 private:
 	//ワールド変換データ
@@ -47,10 +70,8 @@ private:
 	//速度
 	Vector3 velocity_ = { 0.5,0.5,1 };
 
-	//フェーズ
-	Phase phase_ = Phase::Approach;
 
-	//メンバ関数ポインタのテーブル
-	static void (Enemy::* phasePFuncTable[])();
+	//ステート
+	BaseEnemyState* state;
 
 };
